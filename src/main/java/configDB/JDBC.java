232938cdf541ddb1ab22f9e003115237af03bf20
Fylyheng <fylyheng.phally@gmail.com>
2020-01-27 13:28:08 +0700

    update project

--- src/main/java/configDB/JDBC.java
+++ src/main/java/configDB/JDBC.java
@@ -4,10 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBC {
+	
+	/**
+	 * this is default config
+	 */
 	String port="3306";
 	String dbName="libaryDB";
 	String connectionName="root";
 	String password="12345678";
+	
+	
 	public JDBC() {}
 	public JDBC(String port, String dbName, String connectionName,String password) {
 		this.port = port;
@@ -17,14 +23,23 @@ public class JDBC {
 		
 	}
 
-	public void connection() throws Exception {
+	
+	/**
+	 * method connection
+	 * returnType is Connection
+	 * @return
+	 */
+	public Connection connection() {
+		
+		Connection cnn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection cnn = DriverManager.getConnection("jdbc:mysql://localhost:"+port+"/"+dbName+"", connectionName, password);
+			cnn = DriverManager.getConnection("jdbc:mysql://localhost:"+port+"/"+dbName+"", connectionName, password);
 		} catch (Exception e) {
-			// TODO: handle exception
-			throw e;
+			e.getMessage();
 		}
+		
+		return cnn;
 	}
 
 }
--- src/main/java/configDB/Main.java
+++ src/main/java/configDB/Main.java
@@ -1,10 +1,23 @@
 package configDB;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 	
-	public static void main(String args[]) throws Exception {
-		JDBC conn = new JDBC();
-		conn.connection();
+	public static void main(String args[]) throws SQLException {
+		Statement statement;
+		ResultSet resultSet;
+		JDBC jdbc = new JDBC();
+		jdbc.connection();
+		System.out.println(jdbc.connection().getClass());
+		statement = jdbc.connection().createStatement();
+		resultSet = statement.executeQuery("select * from subject");
+		
+		while(resultSet.next()) {
+			System.out.println(resultSet.getString("code"));
+		}
 	}
 
 

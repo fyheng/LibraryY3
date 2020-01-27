d379aa88bce65a9be8f6e3e12f9904e7b655bb53
Heng <hengta56@gmail.com>
2020-01-27 22:07:48 +0700

    update configDB

--- src/main/java/configDB/JDBC.java
+++ src/main/java/configDB/JDBC.java
@@ -1,98 +1,108 @@
 package configDB;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Map;
 
 public class JDBC {
-
+	
+	public JDBC() {}
 	static Statement statement;
 	static ResultSet resultSet;
 
 	/**
-	 * this is default config
+	 * this is default configuration
 	 */
-	static String port = "3306";
+	static String port = "3307";
 	static String dbName = "libaryDB";
 	static String connectionName = "root";
-	static String password = "12345678";
+	static String password = "12345";
 
-	public JDBC() {
-	}
-	/*
-	 * public JDBC(String tbName) { JDBC.tbName = tbName; }
-	 */
-
+	
 	public JDBC(String port, String dbName, String connectionName, String password) {
-		this.port = port;
-		this.dbName = dbName;
-		this.connectionName = connectionName;
-		this.password = password;
+		JDBC.port = port; // JDBC mean this
+		JDBC.dbName = dbName;
+		JDBC.connectionName = connectionName;
+		JDBC.password = password;
 	}
 
-	/**
-	 * method connection returnType is Connection
-	 * 
-	 * @return
-	 * @throws SQLException
-	 */
-
+	
+	//method connection returnType is Connection
 	public static Connection connection() throws SQLException {
 
 		Connection cnn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			cnn = DriverManager.getConnection("jdbc:mysql://localhost:" + port + "/" + dbName + "", connectionName,
-					password);
+			cnn = DriverManager.getConnection("jdbc:mysql://localhost:" + port + "/" + dbName + "", connectionName,password);
 		} catch (Exception e) {
+			System.out.println("this is error");
 			e.getMessage();
 		}
-
 		return cnn;
 	}
 
+	
+
+	//list all rows with Specified on column
 	public static Object readData(String tbName, String... columnName) throws SQLException {
 
+		ArrayList<String> result = new ArrayList<String>();
 		statement = connection().createStatement();
 		resultSet = statement.executeQuery("select * from " + tbName + "");
 
-		ArrayList<Object> resulft = null;
-		ArrayList<String> count = new ArrayList<String>();
-
-//		for (String name : columnName) {
-//			count.add(name);
-//		}
-
-		System.out.println(count);
-
-		while (resultSet.next()) {
+		//loop rows
+		while (resultSet.next()) {			
+			//loop column
 			for (String name : columnName) {
 				resultSet.getString(name);
-				System.out.print(resultSet.getString(name)+"\t");
-				count.add(resultSet.getString(name));
+				result.add(resultSet.getString(name));
 			}
-			System.out.println("\n");
 		}
-
-		return resulft;
-
+		return result;
 	}
+	
+	
 
-//	public static String[] getColumnName(ResultSet resultSet) throws SQLException {
-//		int columCount = resultSet.getMetaData().getColumnCount();
-//		int i = 1;
-//		String[] columnName = new String[10];
-//		for (i = 1; i <= columCount; i++) 
-//			columnName[i] = resultSet.getMetaData().getColumnName(i);
-//			System.out.println(columnName[i]);
-//			return  columnName;
-//		
-//		System.out.println(columnName);
-//		return columnName;
-//	}
+	//list all rows and all column
+	public static Object readData(String tbName) throws SQLException {
+
+		ArrayList<String> result = new ArrayList<String>();
+		ArrayList<String> columnName = new ArrayList<String>();
+		statement = connection().createStatement();
+		resultSet = statement.executeQuery("select * from " + tbName + "");
+		columnName = JDBC.getColumnName(resultSet);
+
+		//loop rows
+		while (resultSet.next()) {
+			//loop column
+			for (String name : columnName) {
+				resultSet.getString(name);
+				result.add(resultSet.getString(name));
+			}
+		}
+		return result;
+	}
+	
+
+	
+	//get All column name form table
+	public static ArrayList<String> getColumnName(ResultSet resultSet) throws SQLException {
+		int columCount = resultSet.getMetaData().getColumnCount();
+		int i = 1;
+		ArrayList<String> name = new ArrayList<String>();
+		
+		for (i = 1; i <= columCount; i++) {
+			name.add(resultSet.getMetaData().getColumnName(i));
+		}
+		
+		//print column name
+		//System.out.println(name);
+		
+		return name;
+	}
 
 }

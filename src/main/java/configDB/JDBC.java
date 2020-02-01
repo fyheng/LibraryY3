f9a4f2f9367b4c2cb42b19b364741962e3217052
Heng <hengta56@gmail.com>
2020-02-01 23:50:18 +0700

    rename method

--- src/main/java/configDB/JDBC.java
+++ src/main/java/configDB/JDBC.java
@@ -32,7 +32,7 @@ public class JDBC {
 		JDBC.password = password;
 	}
 
-	// method connection returnType is Connection
+	// method connection returnType Connection
 	public static Connection connection() throws SQLException {
 
 		Connection cnn = null;
@@ -47,27 +47,11 @@ public class JDBC {
 		return cnn;
 	}
 
-	// list all rows with Specified on column
-	public static ArrayList<ArrayList<String>> readData(String tbName, String... columnName) throws SQLException {
-
-		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-		ArrayList<String> set;
-		statement = connection().createStatement();
-		resultSet = statement.executeQuery("select * from " + tbName + "");
-
-		// loop rows
-		while (resultSet.next()) {
-			set = new ArrayList<String>();
-			// loop column
-			for (String name : columnName) {
-				set.add(resultSet.getString(name));
-			}
-			result.add(set);
-		}
-		resultSet.close();
-		return result;
-	}
-
+	
+	
+	
+//List Data from DB=========================================================================================================
+	
 	// list all rows and all column
 	public static ArrayList<ArrayList<String>> readData(String tbName) throws SQLException {
 
@@ -91,6 +75,139 @@ public class JDBC {
 		return result;
 	}
 
+	// list all rows with Specified on column
+	public static ArrayList<ArrayList<String>> readData(String tbName, String... columnName) throws SQLException {
+
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		ArrayList<String> set;
+		statement = connection().createStatement();
+		resultSet = statement.executeQuery("select * from " + tbName + "");
+
+		// loop rows
+		while (resultSet.next()) {
+			set = new ArrayList<String>();
+			// loop column
+			for (String name : columnName) {
+				set.add(resultSet.getString(name));
+			}
+			result.add(set);
+		}
+		resultSet.close();
+		return result;
+	}
+
+	// list all rows with condition
+	public static ArrayList<ArrayList<String>> readBy(String tbName,String column,String value) throws SQLException {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		ArrayList<String> set;
+		ArrayList<String> columnName = new ArrayList<String>();
+		value = value.length()==0 ? JDBC.getDate():value;
+		
+		statement = connection().createStatement();
+		resultSet = statement.executeQuery("select * from " + tbName + " where " + column + " = '" + value + "' ");
+		columnName = JDBC.getColumnName(resultSet);
+
+		// loop rows
+		while (resultSet.next()) {
+			// loop column
+			set = new ArrayList<String>();
+			for (String name : columnName) {
+				set.add(resultSet.getString(name));
+			}
+			result.add(set);
+		}
+		resultSet.close();
+		return result;
+	}
+	
+	// list all rows with 2 condition
+	public static ArrayList<ArrayList<String>> readBys(String tbName,String column1,String value1,String column2,String value2) throws SQLException {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		ArrayList<String> set;
+		ArrayList<String> columnName = new ArrayList<String>();
+		statement = connection().createStatement();
+		resultSet = statement.executeQuery("select * from " + tbName + " where " + column1 + " = '" + value1 + "' and "+column2+ " = '" +value2+"' " );
+		columnName = JDBC.getColumnName(resultSet);
+
+		// loop rows
+		while (resultSet.next()) {
+			// loop column
+			set = new ArrayList<String>();
+			for (String name : columnName) {
+				set.add(resultSet.getString(name));
+			}
+			result.add(set);
+		}
+		resultSet.close();
+		return result;
+	}
+	
+
+//get totals=============================================================================================================
+	
+	//return totals rows from table
+	public static int getCount(String tbName) throws SQLException {
+		int count = 0;
+		statement = connection().createStatement();
+		resultSet = statement.executeQuery("SELECT COUNT(*) as totals from " + tbName + "");
+
+		while (resultSet.next()) {
+			count += resultSet.getInt("totals");
+		}
+		resultSet.close();
+		return count;
+	}
+
+	//return totals rows by column from table
+	public static int getCount(String tbName, String colName, String value) throws SQLException {
+		int c = 0;
+		statement = connection().createStatement();
+		resultSet = statement
+				.executeQuery("select count(id) as totals from " + tbName + " where " + colName + " = '" + value + "' ");
+		while (resultSet.next()) {
+			c += resultSet.getInt("totals");
+		}
+		resultSet.close();
+		return c;
+
+	}
+
+	
+	//return totals rows by 2 condition
+	public static int getCountBy(String tbName, String c1, String v1, String c2, String v2) throws SQLException {
+		int c = 0;
+		statement = connection().createStatement();
+		resultSet = statement
+				.executeQuery("select count(id) as totals from " + tbName + " where " + c1 + " = '" + v1 + "' and "+c2+" = '"+v2+"' ");
+		while (resultSet.next()) {
+			c += resultSet.getInt("totals");
+		}
+		resultSet.close();
+		return c;
+	}
+	
+	
+	//return totals rows by current date
+	public static int getCountToday(String tbName, String colDateName) throws SQLException {
+		int c = 0;
+		String date = JDBC.getDate();
+		statement = connection().createStatement();
+		resultSet = statement
+				.executeQuery("select count(id) as totals from " + tbName + " where " + colDateName + " = '" + date + "' ");
+		while (resultSet.next()) {
+			c += resultSet.getInt("totals");
+		}
+		resultSet.close();
+		return c;
+
+	}
+
+	
+	
+	
+	
+ //@supProcess===================================================================================================
+	
 	// get All column name form table
 	public static ArrayList<String> getColumnName(ResultSet resultSet) throws SQLException {
 		int columCount = resultSet.getMetaData().getColumnCount();
@@ -107,66 +224,7 @@ public class JDBC {
 		return name;
 	}
 
-	public static int getCount(String tbName) throws SQLException {
-		int count = 0;
-		statement = connection().createStatement();
-		resultSet = statement.executeQuery("SELECT COUNT(*) as totals from " + tbName + "");
-
-		while (resultSet.next()) {
-			count +=resultSet.getInt("totals");
-		}
-		resultSet.close();
-		return count;
-	}
-
-	
-	public static int getCount(String tbName, String col, String value) throws SQLException {
-		int c = 0;
-		statement = connection().createStatement();
-		resultSet = statement.executeQuery(
-				"select count(id) as totals from " + tbName + " where " + col + " = '"+ value +"' ");
-		while (resultSet.next()) {
-			c += resultSet.getInt("totals");
-		}
-		resultSet.close();
-		return c;
-
-	}
-	
-	/**
-	 * @param tbName
-	 * @param col column to find
-	 * @return totals rows where value current date
-	 */
-	public static int getCountToday(String tbName, String col) throws SQLException {
-		int c = 0;
-		String date =  JDBC.getDate();
-		statement = connection().createStatement();
-		resultSet = statement.executeQuery(
-				"select count(id) as totals from " + tbName + " where " + col + " = '"+ date +"' ");
-		while (resultSet.next()) {
-			c += resultSet.getInt("totals");
-		}
-		resultSet.close();
-		return c;
-
-	}
-	
-	public static int getCount(String tbName, String c1, String c2, String v1,String v2) throws SQLException {
-		int c = 0;
-		String date =  JDBC.getDate();
-		statement = connection().createStatement();
-		resultSet = statement.executeQuery(
-				"select count(id) as totals from " + tbName + " where " + c1 + " = '"+ v1 +"' ");
-		while (resultSet.next()) {
-			c += resultSet.getInt("totals");
-		}
-		resultSet.close();
-		return c;
-
-	}
-
-	//getCurrent date java.lang 
+	// getCurrent date java.lang
 	public static String getDate() {
 		// DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDateTime now = LocalDateTime.now();
--- src/main/java/configDB/Main.java
+++ src/main/java/configDB/Main.java
@@ -4,16 +4,21 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
-	
+
 	public static void main(String args[]) throws SQLException {
-		
-		ArrayList<ArrayList<String>> reauft = JDBC.readData("book_detail","code","dueDate");
-		System.out.println(reauft.size());
-		
-		
-		int c = JDBC.getCount("book_detail","dueDate","2020-2-01");
+
+		ArrayList<ArrayList<String>> result = JDBC.readData("book_detail", "code", "dueDate");
+
+		String value = JDBC.getDate();
+		System.out.println(value);
+		result = JDBC.readBy("book_detail", "dueDate","2012-01-01");
+
+		result = JDBC.readBys("book_detail", "status", "2", "dueDate", "2020-02-02");
+
+		System.out.println(result);
+
+		int c = JDBC.getCountBy("book_detail","status","2","dueDate","2020-02-02");
 		System.out.println(c);
 	}
-
 
 }

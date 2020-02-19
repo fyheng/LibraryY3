d8f389733977ee3b0613517785d99c2a1c82bfb8
Heng <hengta56@gmail.com>
2020-02-19 08:40:30 +0700

    update send main

--- src/main/java/login/Validate.java
+++ src/main/java/login/Validate.java
@@ -1,8 +1,51 @@
 package login;
 
-public class Validate {
-	
-	static String userName;
-	static String password;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
+public class Validate {
+
+	 static String userName = "Ly HenG";
+	 static String password = "heng0000";
+
+	public static void sendMail() {
+
+		String to = "fylyheng10@gmail.com";
+		String from = "hengta56@gmail.com";
+
+		// Get system properties
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.host", "smtp.mailtrap.io");
+		prop.put("mail.smtp.port", "25");
+		prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+
+		// Get the default Session object.
+		Session session = Session.getInstance(prop, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		});
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("This is the Subject Line!");
+			message.setText("This is actual message");
+
+			// Send message
+			Transport.send(message);
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
 }
